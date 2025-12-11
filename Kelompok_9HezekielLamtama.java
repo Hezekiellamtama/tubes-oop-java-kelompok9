a3b15e9f205cdf427ce698e339c3dd45dc26e443
Hezekiel Berkat Lamtama <hezekiellamtama@gmail.com>
2025-12-11 12:59:30 +0700

    feat: tambahkan class Poll untuk data voting

--- Kelompok_9HezekielLamtama.java
+++ Kelompok_9HezekielLamtama.java
@@ -1,1 +1,15 @@
-Membuat File Nama Hezekiel Berkat Lamtama
+class Poll {
+    String title;
+    int totalVotes; 
+
+    public Poll(String title) {
+        this.title = title;
+        this.totalVotes = 0;
+    }
+}
+class HezekielLogic {
+    public static void main(String[] args) {
+        // Output sesuai permintaan tugas
+        System.out.println("Saya anggota Hezekiel NIM 12345678"); 
+    }
+}
